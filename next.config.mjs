25f0bdfd935520d350909143d7fc7540eb0deb07
rajd1005 <rajdey.dey1@gmail.com>
2026-03-12 00:42:17 +0530

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  scope: "/",
+  sw: "service-worker.js",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default withPWA(nextConfig);
